19463d1695a33ac84e50a521105ac38e3e2876fb
ConnorFieldUser <conquest.field@gmail.com>
2016-10-04 15:10:18 -0400

    conservo

---
+++ sports_django/table_creater/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Fighter(models.Model):
+
+    rank = models.IntegerField()
+    last_name = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=15)
+    fights = models.IntegerField()
+    strikes = models.IntegerField()
+    str_accuracy = models.IntegerField()
+    takedowns = models.IntegerField()
+    td_accuracy = models.IntegerField()
+    knockdowns = models.IntegerField()
+    reversals = models.IntegerField()
+    passes = models.IntegerField()
+    submissions = models.IntegerField()
+
+# class Team(models.Model):
+#     name = models.CharField(max_length=100)
+#     year = models.IntegerField()
+#     captain = models.ForeignKey(Person)
